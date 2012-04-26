068bf58c8b0568cbfcf6b8b5006697c093a93a30
ngn <jeliazkova.nina@gmail.com>
2012-04-26 17:24:33 +0300

    test with toxbank customized example

--- isa2rdf/isa2rdf-cli/src/test/java/org/isa2rdf/cli/AppTest.java
+++ isa2rdf/isa2rdf-cli/src/test/java/org/isa2rdf/cli/AppTest.java
@@ -2,12 +2,12 @@ package org.isa2rdf.cli;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.net.URL;
 
 import junit.framework.Assert;
 import net.toxbank.client.io.rdf.TOXBANK;
 
+import org.isatools.isatab.isaconfigurator.ISAConfigurationSet;
 import org.isatools.tablib.utils.BIIObjectStore;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -46,9 +46,9 @@ public class AppTest  {
 	@org.junit.Test
 	public void testRDF() throws Exception {
 
-	//	ISAConfigurationSet.setConfigPath(getClass().getClassLoader().getResource("isa-tab/config/isa_configurator").getFile());
+		ISAConfigurationSet.setConfigPath(getClass().getClassLoader().getResource("toxbank-config").getFile());
 
-		URL url = getClass().getClassLoader().getResource("isa-tab//BII-I-1");
+		URL url = getClass().getClassLoader().getResource("toxbank//BII-I-1");
 		Assert.assertNotNull(url);
 		String filesPath = url.getFile();
 		IsaClient cli = new IsaClient();
@@ -59,16 +59,18 @@ public class AppTest  {
 		IsaClient.writeStream(model, output, "application/rdf+xml", true);
 		output.close();
 
+		/*
 		Model test = ModelFactory.createOntologyModel();
 		FileReader reader = new FileReader(out);
 		test.read(reader,null);
 		reader.close();
+		*/
 		
 		out = new File(new File(filesPath),"isatab.n3");
 		output = new FileOutputStream(out);
 		IsaClient.writeStream(model, output, "text/n3", true);
 		output.close();
-		
+	
 		print(model);
 
 	}
