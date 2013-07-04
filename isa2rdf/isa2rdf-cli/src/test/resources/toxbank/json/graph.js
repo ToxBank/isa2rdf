825587892234193e9a79af85721801f9cff86e45
ngn <jeliazkova.nina@gmail.com>
2013-07-04 10:17:53 +0300

    json visualisation test

---
+++ isa2rdf/isa2rdf-cli/src/test/resources/toxbank/json/graph.js
@@ -0,0 +1,107 @@
+function drawGraph(data,chartselector) {
+  var width = 960,
+      height = 500;
+
+  var color = d3.scale.category20();
+
+  var fisheye = d3.fisheye.circular()
+      .radius(120);
+
+  var force = d3.layout.force()
+      .charge(-240)
+      .linkDistance(40)
+      .size([width, height]);
+
+  var svg = d3.select(chartselector).append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  svg.append("rect")
+      .attr("class", "background")
+      .attr("width", width)
+      .attr("height", height);
+
+    var n = data.nodes.length;
+
+    force.nodes(data.nodes).links(data.links);
+
+    // Initialize the positions deterministically, for better results.
+    data.nodes.forEach(function(d, i) { d.x = d.y = width / n * i; });
+
+    // Run the layout a fixed number of times.
+    // The ideal number of times scales with graph complexity.
+    // Of course, don't run too long—you'll hang the page!
+    force.start();
+    for (var i = n; i > 0; --i) force.tick();
+    force.stop();
+
+    // Center the nodes in the middle.
+    var ox = 0, oy = 0;
+    data.nodes.forEach(function(d) { ox += d.x, oy += d.y; });
+    ox = ox / n - width / 2, oy = oy / n - height / 2;
+    data.nodes.forEach(function(d) { d.x -= ox, d.y -= oy; });
+
+    var link = svg.selectAll(".link")
+        .data(data.links)
+      .enter().append("line")
+        .attr("class", "link")
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; })
+        .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+/*
+    var node = svg.selectAll(".node")
+        .data(data.nodes)
+      .enter().append("circle")
+        .attr("class", "node")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", 10)
+        .style("fill", function(d) { return color(d.group); })
+        .call(force.drag);
+*/
+
+  var node = svg.selectAll(".node")
+      .data(data.nodes)
+      .enter().append("g")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .call(force.drag);
+
+var circle =  node.append("circle")
+	   .attr("class", "node")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", 10)
+        .style("fill", function(d) { return color(d.group); })        .call(force.drag);
+
+
+var text = node.append("text")
+      .attr("x", function(d) { return d.x; })
+      .attr("y", function(d) { return d.y; })
+      .style("color","red")
+      .attr("text-anchor", "end")
+      .text(function(d) { return "CID "+d.name; });
+
+    svg.on("mousemove", function() {
+      fisheye.focus(d3.mouse(this));
+
+      circle.each(function(d) { d.fisheye = fisheye(d); })
+          .attr("cx", function(d) { return d.fisheye.x; })
+          .attr("cy", function(d) { return d.fisheye.y; })
+          .attr("r", function(d) { return d.fisheye.z * 10;});
+
+      text.each(function(d) { d.fisheye = fisheye(d); })
+          .attr("x", function(d) { return d.fisheye.x; })
+          .attr("y", function(d) { return d.fisheye.y; });
+
+      link.attr("x1", function(d) { return d.source.fisheye.x; })
+          .attr("y1", function(d) { return d.source.fisheye.y; })
+          .attr("x2", function(d) { return d.target.fisheye.x; })
+          .attr("y2", function(d) { return d.target.fisheye.y; });
+    });
+  
+}
+
---
+++ isa2rdf/isa2rdf-cli/src/test/resources/toxbank/json/isatab.js
@@ -0,0 +1,159 @@
+function demo(url) {
+	/*
+	$.getJSON("isatab.json", function(data) {
+		$.each(data.study, function(key,study){
+			console.log(study);
+	        $.each(study.processing, function(i,s){
+	            console.log(key + " " + s.input + "," + s.output);
+	        });
+		});
+    }).error(function(jqXhr, textStatus, error) {
+                alert("ERROR: " + textStatus + ", " + error);
+    });
+    */
+	defineProcessingTable(null,url);
+	
+}
+
+
+function defineProcessingTable(root,url) {
+	var oTable = $('#processing').dataTable( {
+		"sAjaxDataProp" : "processing",
+		"bProcessing": true,
+		"bServerSide": false,
+		"bStateSave": false,
+		"aoColumnDefs": [
+ 				{ "mData": function ( source) {
+ 					return source.type; 
+ 				  },
+ 				  "asSorting": [ "asc", "desc" ],
+				  "aTargets": [ 0 ],	
+				  "bSearchable" : true,
+				  "bUseRendered" : false,
+				  "bSortable" : true,
+				  "fnRender" : function(o,val) {
+					  return val;
+				  }
+				},
+				{ "mDataProp": function ( source ) {
+					return source.input==null?"":
+						source.type=='MaterialProcessing'?
+							("["+source.input.id + "." + source.input.material.id + "] " + source.input.material.acc):
+						source.type=='DataAcquisition'?
+							("["+source.input.id + "." +source.input.material.id + "] " + source.input.material.acc):
+							("["+source.input.id + "." +source.input.data.id + "] " + source.input.data.acc);
+				  },
+				  "asSorting": [ "asc", "desc" ],
+				  "aTargets": [ 1 ],	
+				  "bSearchable" : true,
+				  "bSortable" : true
+				},
+				{ "mDataProp": function ( source ) {
+					return source.applies==null?"":
+						("["+source.applies.id + "] " + source.applies.protocol);
+				  },
+				  "asSorting": [ "asc", "desc" ],
+				  "aTargets": [ 2 ],	
+				  "bSearchable" : true,
+				  "bSortable" : true
+				},			
+				{ "mDataProp": function ( source ) {
+					return source.applies==null?"":source.applies.parameters; 
+				  },
+				  "asSorting": [ "asc", "desc" ],
+				  "aTargets": [ 3 ],	
+				  "bSearchable" : true,
+				  "bSortable" : true
+				},				
+				{ "mDataProp": function ( source ) {
+					return source.output==null?"": 
+					source.type=='MaterialProcessing'?
+						("["+source.output.id + "." +source.output.material.id + "] " + source.output.material.acc):
+						("["+source.output.id + "." +source.output.data.id + "] " + source.output.data.acc);
+				  },
+				  "asSorting": [ "asc", "desc" ],
+				  "aTargets": [ 4 ],	
+				  "bSearchable" : true,
+				  "bSortable" : true
+				},
+				
+			],
+		"fnServerData": function ( sSource, aoData, fnCallback, oSettings ) {
+			      oSettings.jqXHR = $.ajax( {
+			        "type": "GET",
+			        "url": sSource ,
+			        "data": aoData,
+			        "dataType": "json", 
+			        "success": function(json) {
+			        	var thestudy = null;
+			        	var graph = {"nodes":[],"links":[]};
+
+			        	$.each(json.study, function(key,study){
+			        		thestudy = study;
+			        		
+			    	        $.each(study.nodes, function(i,s){
+			    	        	if (s.material !=undefined) {
+			    	        		var id = s.material;
+			    	        		s.material = study.materials[id];
+			    	        		s.material['id'] = id;
+			    	        	} else if (s.data !=undefined) {
+			    	        		var id = s.data;
+			    	        		s.data = study.data[id];
+			    	        		s.data['id'] = id;
+			    	        	};
+			    	        	s['index'] = graph.nodes.length;
+			    	        	graph.nodes.push({'name':i,'group':1});
+			    	        });
+			    	        $.each(study.processing, function(i,s){
+			    	        	graph.links.push({
+			    	        		        "source": study.nodes[s.input].index , 
+			    	        				"target": study.nodes[s.output].index,
+			    	        				"value": s.input
+			    	        				});
+			    	        });
+			    	        $.each(study.processing, function(i,s){
+			    	        	var id = s.input;
+		    	        		s.input = study.nodes[s.input];
+		    	        		s.input['id'] = id;
+		    	        		id = s.output;
+		    	        		s.output = study.nodes[s.output];
+		    	        		s.output['id'] = id;
+		    	        		if (s.applies!=undefined) {
+			    	        		var id = s.applies;
+		    	        			s.applies = study.protocolApplications[id];
+		    	        			s.applies['id'] = id;
+		    	        		}
+
+			    	        });
+			    	        
+			    	        
+
+			    	        
+			    	        			    	        
+			    		});
+			        	fnCallback(thestudy);
+			        	drawGraph(graph,'#chart1');
+			        },
+			        "cache": false,
+			        "error" : function( xhr, textStatus, error ) {
+			        	oSettings.oApi._fnProcessingDisplay( oSettings, false );
+			        }
+			      } );
+		},			
+		"sDom" : '<"help remove-bottom"if><"help"p>Trt<"help"l>',	
+		"bJQueryUI" : true,
+		"bPaginate" : true,
+		"sPaginationType": "full_numbers",
+		"sPaginate" : ".dataTables_paginate _paging",
+		"bDeferRender": true,
+		"bSearchable": true,
+		"sAjaxSource": url,
+		"iDisplayLength": 100,
+		"oLanguage": {
+				"sSearch": "Filter:",
+	            "sProcessing": "<img src='"+root+"/images/progress.gif' border='0'>",
+	            "sLoadingRecords": "No records found."
+	    }
+	} );
+	return oTable;
+}
